a8aa9c2c103cb06c19479776c9e0ce3ae38e658e
Haley Lenner <lenner@usc.edu>
2013-03-03 20:33:40 -0800

    commented, bcalg code, and bc.cpp prints to new file, parses input file, check for errors, and used github properly with closing issues, close #26 close #27 close #28 close #29 close #30 close #31

--- bc.cpp
+++ bc.cpp
@@ -9,12 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bcuser.h"
+#include "BCAlg.h"
 
 using namespace std;
+
 
 int main(int argc, char *argv[])
 {
 BCUser x;
+BCAlg *y = new BCAlg();
 gmlwriter g;
 
 if(argc < 3){
@@ -25,7 +28,7 @@ if(argc < 3){
 	vector<string> edges;
 	string gml, output;
 	Mylist<BCUser*> userList;
-
+	
 	if (GMLReader::read(argv[1], nodes, edges)){//reads the input file and puts info to nodes/edges
 		//tests if it can open the file
 	}
@@ -36,43 +39,58 @@ if(argc < 3){
 	string name, first, last;
 	int id, age, zip;
 	string random;
-	for (int i=0; i<nodes.size(); i++){
-		stringstream ss(nodes[i]);
-		for (int x=0; x<4; x++){
+	//this reads in each user and puts their information into the userList
+	for (unsigned int i=0; i<nodes.size(); i++){
+		stringstream ss(nodes[i]);//creates a stringstream for the node at i
+		for (int x=0; x<4; x++){ //this handles each part of a User, and makes it so entering in any order works
 			ss >> random;
 			if (random == "id"){
-				ss >> id;
+				ss >> id;//sets the id
 			}
 			if (random == "name"){
-				ss >> first >> last;
-				name = first +" "+ last;
+				ss >> first>> last;
+				name = first +" "+ last;//sets name
 			}
 			if (random == "age"){
-				ss >> age;
+				ss >> age;//sets to age
 			}
 			if (random == "zip"){
-				ss >> zip;
+				ss >> zip;//sets to zip
 			}
 		}
-		BCUser* auser= new BCUser(id, name, age, zip);
+		BCUser* auser= new BCUser(id, name, age, zip);//creates a user of 
 		userList.push_back(auser);
 	}
 	//below this processes the edges(friend connections) of the userList and creates links/friends based on these edges
 	int source, friends;
 	string definition;
-	for (int i=0; i<edges.size();i++)
+	cout << "Edges size: " <<edges.size()<< endl;
+	for (unsigned int i=0; i<edges.size();i++)
 	{
+	//reads in all edges with a stringstream
 		stringstream xx(edges[i]);
 		xx >> definition;
 		xx >> source;
 		xx >> definition;
 		xx >> friends;
 		if (userList.at(source)){
-		userList.at(source)->makeFriend(friends);
+			userList.at(source)->makeFriend(friends);//makes source/friend friends
 		}
 	}
-	g.write_friends(argv[2], userList);//writes friends to a new file
+	//performs the BC algorithm
+	y->computeBC(userList);
+	
+	//prints the user id and bc score to a new file
+	int z = userList.size(); //stores the size of the userlist
+	ofstream ofile(argv[2]);//takes input file
+	if (ofile.is_open()) { //tests if it is open/good
+		for (int i=0; i<z; i++)//goes through the users
+		{
+			int id= userList.at(i)->getId();//sets id to the user at i
+			ofile << "id:  " << id << "   "; //prints the id
+			ofile<< "bc: " << userList.at(i)->bc << endl;//prints normalized bc score of the user
+		}	
+	}
+	delete y;
   return 0;
 }
-	
-
