d3d09250446a046d0d27d08a261e6a27c915bff7
Haley Lenner <lenner@usc.edu>
2013-04-25 17:08:23 -0700

    changed numsp from int to double, everything works now

--- bcuser.h
+++ bcuser.h
@@ -13,7 +13,7 @@ public:
 	BCUser(int i);
 	~BCUser();
 	int dist; //distance to current source vertex
-	int numsp; //number of shortest paths
+	double numsp; //number of shortest paths
 	Mylist<int> preds; //List of predecessors
 	double delta; //partial BC score
 	double bc; //total BC score
